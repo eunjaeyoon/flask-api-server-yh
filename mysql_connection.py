7f528797ae9d641be66897329851c3128371e694
eunjaeyoon <95923674+eunjaeyoon@users.noreply.github.com>
2022-01-13 14:47:25 +0900

    add

--- mysql_connection.py
+++ mysql_connection.py
@@ -3,8 +3,8 @@ import mysql.connector
 def get_connection() :
     connection = mysql.connector.connect(
         host = 'database-1.cvtuufc0they.ap-northeast-2.rds.amazonaws.com',
-        database = 'test',
-        user = 'admin',
-        password = 'dms230145'
+        database = 'movie_db',
+        user = 'movie_user',
+        password = '2301'
     )
     return 
